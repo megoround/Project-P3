63bfff1a0ac6b05423be6914b8360cb775c5386a
MSDN-SPECIAL\Administrator <megoround@naver.com>
2018-03-06 14:45:08 +0900

    V0

---
+++ Assets/Resources/Scripts/BattleManager.cs
@@ -0,0 +1,415 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Plate
+{
+    public GameObject PlateGO;
+
+    public int isPlayers;
+    public int number;
+    public BattleCharacter OnPlaterChar;
+
+    public GameObject plate;
+    public GameObject border;
+    public GameObject dangeMark;
+
+    public bool isGood;
+    public bool isBad;
+}
+
+public class BattleManager : MonoBehaviour {
+
+    GameObject BattleCanvas;
+
+    GameObject SquarePrefab, SkillSlotPrefab, BattleCharacterPrefab, SkillStackPrefab;
+    
+    GameObject Background;
+
+    GameObject SkillTextWindow;
+
+    Plate[][] Plates;
+    Plate[] PlayerPlate;
+    Plate[] EnemyPlate;
+
+    BattleCharacter[] PlayerCharacters;
+    BattleCharacter[] EnemyCharacters;
+
+    /// ////////////////////////////////////////
+
+    //PlayerCharacter CurrentTurnChar;
+    int CurrentTurnChar = 1;
+
+    //PlayerCharacter 
+
+    // Use this for initialization
+    void Start () {
+
+        ///////////Prefab Set////////
+        SquarePrefab = Resources.Load("Prefabs/" + "BattleSquare") as GameObject;
+        BattleCharacterPrefab = Resources.Load("Prefabs/" + "BattleCharacter") as GameObject;
+        SkillSlotPrefab = Resources.Load("Prefabs/" + "SkillSlot") as GameObject;
+        SkillStackPrefab = Resources.Load("Prefabs/" + "SkillStack") as GameObject;
+
+        BattleCanvas = GameObject.Find("BattleCanvas");
+        SkillTextWindow = GameObject.Find("SkillTextWindow");
+        SkillTextWindow.SetActive(false);
+
+
+        ///////////Plate Set////////
+        PlayerPlate = new Plate[9];
+        for (int i = 0; i < 9; i++)
+            PlayerPlate[i] = new Plate();
+        EnemyPlate = new Plate[9];
+        for (int i = 0; i < 9; i++)
+            EnemyPlate[i] = new Plate();
+
+        Plates = new Plate[2][];
+        Plates[0] = PlayerPlate;
+        Plates[1] = EnemyPlate;
+        
+        PlayerCharacters = new BattleCharacter[6];
+        for (int i = 0; i < 6; i++)
+            PlayerCharacters[i] = new BattleCharacter();
+        EnemyCharacters = new BattleCharacter[6];
+        for (int i = 0; i < 6; i++)
+            EnemyCharacters[i] = new BattleCharacter();
+
+        for (int i=0;i<9;i++)
+        {
+            GameObject Inst = Instantiate(SquarePrefab);
+            Inst.transform.parent = BattleCanvas.transform.Find("CenterArea/Plate_P");
+            Inst.transform.localScale = SquarePrefab.transform.localScale;
+            Inst.transform.localPosition = new Vector3(240, -3, 0);
+            Inst.transform.localPosition += new Vector3(160 * (i % 3) - 160, 160 * (i / 3) - 160, 0);
+
+            Inst.name = "Square_" + (i+1);
+
+            //////////////////////////////////////////////
+
+            PlateInit(PlayerPlate[i], Inst, i);
+        }
+
+        //SkillSlot Set//
+        for (int i=0;i<5;i++)
+        {
+            GameObject Inst = Instantiate(SkillSlotPrefab);
+            Inst.transform.parent = BattleCanvas.transform.Find("Bottom/Status/Skills");
+            Inst.transform.localScale = SquarePrefab.transform.localScale;
+            Inst.transform.localPosition = new Vector3(-490 + 100 * i, 0, 0);
+
+            if(i == 4)
+            {
+                Inst.name = "SkillSlot_" + "Wait";
+                Image Img = Inst.transform.Find("Image").GetComponent<Image>();
+
+                Sprite newSprite = Resources.Load<Sprite>("Images/UI/wait");
+                Img.sprite = newSprite;
+
+                GameObject SkillSlot = Inst;
+                GameObject SkillCDtext = SkillSlot.transform.Find("CDtext").gameObject;
+                GameObject SkillImg = SkillSlot.transform.Find("CDimage").gameObject;
+
+                SkillCDtext.GetComponent<Text>().text = "";
+                SkillImg.GetComponent<Image>().color = new Color(1f, 1f, 1f, 0f);
+
+            }
+            else
+            {
+                Inst.name = "SkillSlot_" + (i + 1);
+
+                EventTrigger.Entry entry = new EventTrigger.Entry();
+                entry.eventID = EventTriggerType.PointerEnter;
+                entry.callback.AddListener((eventData) =>
+                {
+                    SkillTextWindow.SetActive(true);
+                    SkillTextWindow.transform.localPosition = Inst.transform.localPosition;
+                    SkillTextWindow.transform.localPosition += new Vector3(200, -50, 0);
+                    SkillWindowPopup(Inst);
+                });
+                Inst.transform.Find("OnClick").gameObject.GetComponent<EventTrigger>().triggers.Add(entry);
+
+                entry = new EventTrigger.Entry();
+                entry.eventID = EventTriggerType.PointerExit;
+                entry.callback.AddListener((eventData) =>
+                {
+                    SkillTextWindow.SetActive(false);
+                });
+                Inst.transform.Find("OnClick").gameObject.GetComponent<EventTrigger>().triggers.Add(entry);
+
+                entry = new EventTrigger.Entry();
+                entry.eventID = EventTriggerType.PointerClick;
+                entry.callback.AddListener((eventData) =>
+                {
+                    SkillSlotOnClick(Inst.name);
+                });
+                Inst.transform.Find("OnClick").gameObject.GetComponent<EventTrigger>().triggers.Add(entry);
+            }
+        }
+
+
+        ///////////Test Set////////
+
+
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    void Init()
+    {
+        for(int i=0;i< _GameManager.instance.PlayerParty.PartyMemberCount;i++)
+        {
+            Character Char = _GameManager.instance.PlayerParty.Characters[i];
+            Char.Point = i; // temp
+            PlayerCharaterInit(Char, Char.Point, i);
+        }
+
+        StatusRefresh();
+    }
+
+    void PlateInit(Plate plate, GameObject Inst, int i)
+    {
+        plate.PlateGO = Inst;
+        plate.number = i;
+        plate.isBad = false;
+        plate.isGood = false;
+        plate.isPlayers = 0;
+
+        plate.plate = Inst.transform.Find("Plate").gameObject;
+        plate.border = Inst.transform.Find("Border").gameObject;
+        plate.dangeMark = Inst.transform.Find("DangerMark").gameObject;
+
+        plate.OnPlaterChar = null;
+
+        plate.plate.GetComponent<Image>().color = new Color(1f, 1f, 1f, 0f);
+    }
+
+    void PlayerCharaterInit(Character characterData, int plate, int number)  //임시
+    {
+        GameObject Inst = Instantiate(BattleCharacterPrefab);
+        Image Img = Inst.GetComponent<Image>().GetComponent<Image>();
+        Sprite[] newSprite = Resources.LoadAll<Sprite>("Images/Char/" + characterData.CharImage);
+        Img.sprite = newSprite[characterData.CharImageNumber];
+
+        //
+        BattleCharacter character = new BattleCharacter(characterData);
+
+        character.BattleCharacterGO = Inst;
+        character.OnPlate = Plates[0][plate];
+        character.isPlayers = 0;
+
+        PlayerCharacters[number] = character;
+
+        CharacterOnPlate(character.OnPlate, character);
+        HPRefreshOnSquare(character);
+        //Debug.Log(number + " Max : "+PlayerCharacters[number].MaxHP);
+        //Debug.Log("Current : " + PlayerCharacters[number].CurrentHP);
+    }
+
+    void HPRefreshOnSquare(BattleCharacter character)
+    {
+        GameObject redbar;
+        float HPremainPer;
+
+        HPremainPer = 100 - ((float)character.CurrentHP / (float)character.MaxHP)*100;
+        
+        redbar = character.BattleCharacterGO.transform.Find("HPbar/HPred").gameObject;
+        redbar.transform.localPosition = new Vector3(-HPremainPer, -9,0);
+    }
+
+    void StatusRefresh()
+    {
+        GameObject FaceImage, NameText, Skills, redbar, hpText;
+        BattleCharacter character = PlayerCharacters[CurrentTurnChar];
+        float HPremainPer;
+
+        /////////////////////////////////////////
+
+        //HP 갱신
+        HPremainPer = 225 - ((float)character.CurrentHP / (float)character.MaxHP) * 225;
+        redbar = GameObject.Find("Bots/HP/HPbar/HPred");
+        redbar.transform.localPosition = new Vector3(-HPremainPer, 0, 0);
+        hpText = GameObject.Find("Bots/HP/HPbar/Text");
+        hpText.GetComponent<Text>().text = (int)character.CurrentHP + " / " + (int)character.MaxHP;
+
+        //이미지, 이름 갱신
+        FaceImage = GameObject.Find("Status/Face/Image");
+        NameText = GameObject.Find("Status/Name/Text");
+        
+        Image Img = FaceImage.GetComponent<Image>();
+        Sprite[] newSprite = Resources.LoadAll<Sprite>("Images/Face/" + character.FaceImage);
+        Img.sprite = newSprite[character.FaceImageNumber];
+
+        NameText.GetComponent<Text>().text = character.Name;
+
+        //스킬창 갱신
+        Skills = GameObject.Find("Status/Skills");
+
+        GameObject SkillSlot,SkillImg,SkillCDtext,SkillCDimage;
+        for (int i=0;i<4;i++)
+        {
+            SkillSlot = Skills.transform.Find("SkillSlot_" + (i+1)).gameObject;
+            SkillImg = SkillSlot.transform.Find("Image").gameObject;
+
+            Img = SkillImg.GetComponent<Image>();
+            Debug.Log(character.Skills[i].Image);
+            newSprite = Resources.LoadAll<Sprite>("Images/UI/Icons/" + character.Skills[i].Image);
+            Img.sprite = newSprite[character.Skills[i].ImageNumber];
+
+            SkillCDtext = SkillSlot.transform.Find("CDtext").gameObject;
+            SkillCDimage = SkillSlot.transform.Find("CDimage").gameObject;
+
+            if(character.Skills[i].CurrentSkillCD == 0)
+            {
+                SkillCDtext.GetComponent<Text>().text = "";
+                SkillCDimage.GetComponent<Image>().color = new Color(1f, 1f, 1f, 0f);
+            }
+            else
+            {
+                SkillCDtext.GetComponent<Text>().text = "" + character.Skills[i].CurrentSkillCD;
+                SkillCDimage.GetComponent<Image>().color = new Color(0.35f, 0.35f, 0.35f, 0.5f);
+            }
+        }
+
+    }
+
+    void Effect(int plateNumber,int amount,int TargetAreaNumber) // temp
+    {
+        BattleCharacter Char = Plates[TargetAreaNumber][plateNumber-1].OnPlaterChar;
+        
+        if (Char == null)
+            return;
+        else
+        {
+            Char.CurrentHP += amount;
+            if (Char.CurrentHP > Char.MaxHP)
+                Char.CurrentHP = Char.MaxHP;
+            else if (Char.CurrentHP < 0)
+                Char.CurrentHP = 0;
+
+            HPRefreshOnSquare(Char);
+            if (Char.Equals(PlayerCharacters[CurrentTurnChar]))
+            {
+                StatusRefresh();
+            }
+        }
+
+    }
+    void CharacterOnPlate(Plate plate, BattleCharacter Char)
+    {
+        plate.OnPlaterChar = Char;
+
+        Char.BattleCharacterGO.transform.parent = Char.OnPlate.PlateGO.transform;
+        Char.BattleCharacterGO.transform.localPosition = new Vector2(0, -12);
+        Char.BattleCharacterGO.transform.localScale = BattleCharacterPrefab.transform.localScale;
+    }
+
+    void SkillSlotOnClick(string Name)
+    {
+        string[] spt = Name.Split('_');
+        int numb = int.Parse(spt[1]);
+
+        BattleStack BattleStackInfo = new BattleStack();
+        BattleStackInfo.User = PlayerCharacters[CurrentTurnChar];
+        BattleStackInfo.SkillOnStack = BattleStackInfo.User.Skills[numb];
+        BattleStackInfo.CastPlate = 1;
+        BattleStackInfo.CastPoint = 4;
+
+        GameObject Inst = Instantiate(SkillStackPrefab);
+        //Inst.transform.parent = GameObject.Find()
+    }
+
+    void SkillWindowPopup(GameObject Name)
+    {
+        string[] substring = Name.name.Split('_');
+        int number = int.Parse(substring[1]);
+        Debug.Log("" + name);
+        Skill Selected = PlayerCharacters[CurrentTurnChar].Skills[number - 1];
+
+        string PhaseColor = "";
+        switch (Selected.Phase)
+        {
+            case 0: PhaseColor = "<color=#00ff00>"; break; //prep
+            case 1: PhaseColor = "<color=#ffff00>"; break; //dash
+            case 2: PhaseColor = "<color=#ff0000>"; break; //blast  
+        }
+
+        SkillTextWindow.transform.Find("SkillMainWindow/Name").gameObject.GetComponent<Text>().text = PhaseColor + Selected.Name + "</color>";
+        SkillTextWindow.transform.Find("SkillMainWindow/Text").gameObject.GetComponent<Text>().text = Selected.Text;
+        SkillTextWindow.transform.Find("SkillMainWindow/SkillCD/CDtext").gameObject.GetComponent<Text>().text = "" + Selected.SkillCD;
+
+        if (Selected.isFree == 1)
+            SkillTextWindow.transform.Find("SkillMainWindow/Free").gameObject.SetActive(true);
+        else
+            SkillTextWindow.transform.Find("SkillMainWindow/Free").gameObject.SetActive(false);
+
+        ///
+        
+        for (int i=0;i<3;i++)
+        {
+            GameObject TargetSquare = 
+            SkillTextWindow.transform.Find("SkillSubWindow/Area1/CastAreaSquare/Square" + (i + 1)).gameObject;
+
+            if (Selected.CastArea[i] == 1)
+                TargetSquare.GetComponent<Image>().color = Color.blue;
+            else
+                TargetSquare.GetComponent<Image>().color = Color.gray;
+        }
+        for (int i = 0; i < 9; i++)
+        {
+            GameObject TargetSquare =
+            SkillTextWindow.transform.Find("SkillSubWindow/Area2/TargetAreaSquare/Square" + (i + 1)).gameObject;
+
+            if (Selected.TargetArea[i] == 1)
+            {
+                if (Selected.isHealing == 1)
+                    TargetSquare.GetComponent<Image>().color = Color.green;
+                else
+                    TargetSquare.GetComponent<Image>().color = Color.red;
+            }
+            else
+                TargetSquare.GetComponent<Image>().color = Color.gray;
+        }
+        for (int i = 0; i < 9; i++)
+        {
+            GameObject TargetSquare =
+            SkillTextWindow.transform.Find("SkillSubWindow/Area3/EffectAreaSquare/Square" + (i + 1)).gameObject;
+
+            if (Selected.EffectArea[i] == 1)
+            {
+                if(Selected.isHealing == 1)
+                    TargetSquare.GetComponent<Image>().color = Color.green;
+                else
+                    TargetSquare.GetComponent<Image>().color = Color.red;
+            }
+            else
+                TargetSquare.GetComponent<Image>().color = Color.gray;
+        }
+        //
+        SkillTextWindow.transform.Find("SkillSubWindow/Area4/Window/EffectText").gameObject.GetComponent<Text>().text = Selected.ExtraText;
+    }
+
+    class BattleStackManager
+    {
+        LinkedList<BattleStack> Stack_PREP;
+        LinkedList<BattleStack> Stack_DASH;
+        LinkedList<BattleStack> Stack_BLAST;
+
+        LinkedList<Skill> Stack_PlayerAct;
+    }
+
+
+    class BattleStack
+    {
+        public Character User;
+        public Skill SkillOnStack;
+        public int CastPlate;      //0 -> 플레이어 , 1 -> 상대
+        public int CastPoint;  //사용한 위치
+    }
+}
---
+++ Assets/Resources/Scripts/_GameManager.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Xml;
+
+
+public class _GameManager : MonoBehaviour
+{
+    public static _GameManager instance = null;
+    public GameObject Loading;
+
+    // Use this for initialization
+    void Start () {
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);//다른 씬을 로드해도 파괴되지 않음.
+
+        OpenMainStage("BT1");
+
+        Init();
+    }
+	
+    public void OpenMainStage(string NextScene)
+    {
+        StartCoroutine(LoadMainScene(NextScene));
+    }
+
+    public IEnumerator LoadMainScene(string NextScene)
+    {
+        Loading.SetActive(true);
+
+        yield return SceneManager.LoadSceneAsync(NextScene, LoadSceneMode.Single); //로딩 다되고 나면 수행
+        //yield return SceneManager.LoadSceneAsync("Stage0", LoadSceneMode.Single);
+
+    }
+
+    void Init()
+    {
+        PlayerParty = new PlayerPartyClass();
+        PlayerParty.Characters = new Character[PlayerParty.PartyMemberCount];
+        for(int i=0;i< PlayerParty.PartyMemberCount;i++)
+        {
+            PlayerParty.Characters[i] = new Character();
+            PlayerParty.Characters[i].Skills = new Skill[4];
+            for(int j=0;j<4;j++)
+            {
+                PlayerParty.Characters[i].Skills[j] = new Skill();
+                PlayerParty.Characters[i].Skills[j].Name = "Alpha" + j;
+
+                PlayerParty.Characters[i].Skills[j].Phase = 2; // 0 : PREP, 1 : DASH, 2 : BLAST
+                PlayerParty.Characters[i].Skills[j].isFree = 0; // 1이면 Free Action
+                PlayerParty.Characters[i].Skills[j].isHealing = 0; // 1이면 Heal
+
+                PlayerParty.Characters[i].Skills[j].CastArea = new int[3]{ 1 , 1 , 0};
+                PlayerParty.Characters[i].Skills[j].TargetArea = 
+                    new int[9] { 0, 1, 1,
+                                 0, 1, 0,
+                                 1, 1, 0 }; //거꾸로
+
+                PlayerParty.Characters[i].Skills[j].EffectArea =
+                    new int[9] { 0, 1, 0,
+                                 1, 1, 1,
+                                 0, 1, 0 };
+
+                PlayerParty.Characters[i].Skills[j].ExtraText = "Free, Heal";
+            }
+        }
+
+        //
+
+        PlayerParty.Characters[1].Name = "Iye";
+        PlayerParty.Characters[1].FaceImageNumber = 1;
+        PlayerParty.Characters[1].CharImageNumber = 35;
+        PlayerParty.Characters[2].Name = "Wynn";
+        PlayerParty.Characters[2].FaceImageNumber = 2;
+        PlayerParty.Characters[2].CharImageNumber = 38;
+        PlayerParty.Characters[3].Name = "Lou";
+        PlayerParty.Characters[3].FaceImageNumber = 3;
+        PlayerParty.Characters[3].CharImageNumber = 41;
+
+    }
+
+    public PlayerPartyClass PlayerParty;
+}
+
+
+public class PlayerPartyClass
+{
+    public int PartyMemberCount = 4;
+    public Character[] Characters;
+}
+public class Character
+{
+    //
+    public int Numb = 0;
+    public string Name = "Aterial";
+    public string FaceImage = "Face_Actor1";
+    public int FaceImageNumber = 0;
+    public string CharImage = "Char_Actor1";
+    public int CharImageNumber = 25;
+    public Skill[] Skills;
+
+    //
+    public int Level = 1;
+    public int MaxHP = 100;
+    public int CurrentHP = 80;
+    public int Point = 1;
+}
+public class Skill
+{
+    public int Numb = 0;
+    public string Name = "Swing";
+    public string Text = ":X";
+    public string Image = "Sample";
+    public int ImageNumber = 10;
+
+    public int SkillCD = 2;
+    public int CurrentSkillCD = 0;
+
+    public SkillTrait[] SkillTraits;
+    public int SelectedTraitNumber;
+
+    //
+
+    public int Phase = 2; // 0 : PREP, 1 : DASH, 2 : BLAST
+    public int isFree = 0; // 1이면 Free Action
+    public int isHealing = 0; // 1이면 회복기 -> 초록색
+
+    public int[] CastArea; // 0 후열 1 중열 2 전열
+    public int[] TargetArea; // 000 / 000 / 000 
+    public int[] EffectArea; // 000 / 000 / 000
+
+    public string ExtraText = "Do Nothing";
+}
+public class SkillTrait
+{
+    public int Numb = 0;
+    public string Name = "Soft";
+    public string Text = ":D";
+    public string Image = "Sample";
+    public int ImageNumber = 11;
+}
