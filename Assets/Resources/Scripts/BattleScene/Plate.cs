b8e1861ebac234f9f6da3cb7fb0bc88d715d4cfd
KJA01 <megoround@naver.com>
2018-03-06 16:46:59 +0900

    Day 0

---
+++ Assets/Resources/Scripts/BattleScene/Plate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plate : MonoBehaviour {
+
+    public bool isPlayers;
+    public GameObject OnPlaterChar;
+
+    public GameObject plate;
+    public GameObject border;
+    public GameObject dangeMark;
+
+    public bool isGood;
+    public bool isBad;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
